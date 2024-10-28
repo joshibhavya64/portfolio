34813ffa7e017ea98505dcfb573f0703c2d4b4c3
Bhavya <joshibhavya64@gmail.com>
2024-10-28 16:48:29 +0530

    footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
 
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light text-white">
-          Copyright © 2024 Bhavya
+          Copyright © 2024 Bhavya Joshi
         </p>
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((profile) => (
