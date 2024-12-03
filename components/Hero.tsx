18852362c9ad4bb8fd85ab79b2f31d7edd65b95d
Bhavya <joshibhavya64@gmail.com>
2024-12-03 15:23:27 +0530

    improvements

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,6 @@
 import { FaLocationArrow } from "react-icons/fa";
 import MagicButton from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
-import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
@@ -21,18 +20,13 @@ const Hero = () => {
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
-      z
+
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center text-white">
-          {/* <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic web magic with Next.js
-          </h2> */}
-
-          <TextGenerateEffect
-            className="text-center text-[40px] md:text-5xl lg:text-6xl text-white"
-            words="Transforming Concepts into Seamless User Experiences"
-            wordClassName="text-white"
-          />
+          <div className="text-center text-[40px] md:text-5xl lg:text-[3.2rem] !leading-snug tracking-wide font-bold text-white my-5">
+            Transforming Concepts into Seamless{" "}
+            <span className="text-purple"> User Experiences </span>
+          </div>
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Bhavya, a Web Developer based in India
